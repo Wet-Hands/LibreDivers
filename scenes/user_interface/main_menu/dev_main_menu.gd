df864a6fcc7701bf5beb47ced683797c782e1bcd
Ian Day <ian.day@mailbox.org>
2026-05-17 03:01:02 -0400

    Update Developed Main Menu

--- scenes/user_interface/main_menu/dev_main_menu.gd
+++ scenes/user_interface/main_menu/dev_main_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+# TODO This could be revamped into a modular menu creation node, however it is 3am rn and I am NOT doing that
+
+@export var debug : bool = false
+
 @export var container_container : Container
 @export var main_container : Container
 
@@ -9,6 +13,7 @@ func _ready() -> void:
 	for child in main_container.get_children():
 		if child is Button:
 			child.connect("pressed", on_button_pressed.bind(child))
+			if debug: print("Connected Pressed Signal: ", str(child))
 		else: push_warning("Incompatible node in MainContainer: ", child)
 
 # Trying something new with main menu code, idk if I like it
@@ -30,3 +35,4 @@ func set_menu_to_container(container : Container) -> void:
 		if child is Container:
 			child.hide()
 	container.show()
+	if debug: print("New Container: ", str(container))
